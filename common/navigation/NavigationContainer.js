05c9b1e932ee858dc49bda7f181027586b551eff
Jeff C <blackone787@gmail.com>
2019-01-07 06:02:22 -0600

    Add AddCard to StackNavigation.

--- common/navigation/NavigationContainer.js
+++ common/navigation/NavigationContainer.js
@@ -1,16 +1,49 @@
-import { createStackNavigator, createAppContainer } from 'react-navigation';
-import { DeckList, DeckDetail } from '../../features/deck';
+import {
+  createStackNavigator,
+  createAppContainer,
+  createBottomTabNavigator
+} from 'react-navigation';
+import { DeckList, DeckDetail, AddDeck } from '../../features/deck';
+import { AddCard } from '../../features/card';
+
+const Tabs = createBottomTabNavigator(
+  {
+    DeckList: {
+      screen: DeckList,
+      navigationOptions: {
+        tabBarLabel: 'Deck List'
+      }
+    },
+    AddDeck: {
+      screen: AddDeck,
+      navigationOptions: {
+        tabBarLabel: 'Add Deck'
+      }
+    }
+  },
+  {
+    tabBarOptions: {
+      activeTintColor: '#FFF',
+      inactiveTintColor: '#CCC'
+    }
+  }
+);
 
 const AppNavigator = createStackNavigator(
   {
-    DeckList: {
-      screen: DeckList
+    Home: {
+      screen: Tabs
     },
+
     DeckDetail: {
       screen: DeckDetail
+    },
+
+    AddCard: {
+      screen: AddCard
     }
   },
-  { initialRouteName: 'DeckList' }
+  { initialRouteName: 'Home' }
 );
 
 export default createAppContainer(AppNavigator);
